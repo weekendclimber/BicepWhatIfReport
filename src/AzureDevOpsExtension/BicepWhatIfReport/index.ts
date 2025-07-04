3e3acb4b02b86b4eb02daac3eb10ccd53402964a
Scott Anderson <scoanderson@soundphysicians.com>
2025-07-03 18:03:18 -0700

    Commented out the file reading stuff.

--- src/AzureDevOpsExtension/BicepWhatIfReport/index.ts
+++ src/AzureDevOpsExtension/BicepWhatIfReport/index.ts
@@ -1,38 +1,36 @@
-// Entry point for Azure DevOps Extension
-//import tl = require('azure-pipelines-task-lib/task');
-import * as fs from 'fs';
-import * as path from 'path';
 
-//import md = require('azure-devops-ui/Markdown');
+//import * as fs from 'fs';
+//import * as path from 'path';
+
+// Entry point for Azure DevOps Extension
+import tl = require('azure-pipelines-task-lib/task');
 import { parseWhatIfJson } from './services/parseWhatIfJson';
 import { generateReport } from './reports/generateReport';
 
 async function main() {
   try {
-    const inputFilePath = path.resolve(__dirname, '../../../tests/AzureDevOpsExtension/report.json');
-    const outputFilePath = path.resolve(__dirname, 'test.md');
-    const inputString = fs.readFileSync(inputFilePath, 'utf8');
-    //const inputString: string | undefined = tl.getInput('bicepWhatIfJSON', true);
-    //if (inputString == 'bad') {
-    //  tl.setResult(tl.TaskResult.Failed, 'Bad input was given');
-    //  return;
-    //}
-    //console.log('Hello', inputString);
+    //const inputFilePath = path.resolve(__dirname, '../../../tests/AzureDevOpsExtension/report.json');
+    //const outputFilePath = path.resolve(__dirname, 'test.md');
+    //const inputString = fs.readFileSync(inputFilePath, 'utf8');
+    const inputString: string | undefined = tl.getInput('bicepWhatIfJSON', true);
+    if (inputString == 'bad') {
+      tl.setResult(tl.TaskResult.Failed, 'Bad input was given');
+      return;
+    }
 
-  // Parse the what-if JSON
-  const parsed = parseWhatIfJson(inputString as string);
-  //console.log('Parsed what-if JSON:', parsed);
+    // Parse the what-if JSON
+    const parsed = parseWhatIfJson(inputString as string);
+    //console.log('Parsed what-if JSON:', parsed);
   
-  // Generate a human-readable report
-  const report = generateReport(parsed);
-  //const report = generateReport(inputString as string);
-  //console.log('Generated report:', report);
-  fs.writeFileSync(outputFilePath, await report, 'utf-8');
-  
+    // Generate a human-readable report
+    const report = generateReport(parsed);
+    //console.log('Generated report:', report);
+    
+    //fs.writeFileSync(outputFilePath, await report, 'utf-8');
+    return tl.setResult(tl.TaskResult.Succeeded, await report);
   } catch (err:any) {
     console.error('Error:', err.message);
-    process.exit(1);
-    //tl.setResult(tl.TaskResult.Failed, err.message);
+    tl.setResult(tl.TaskResult.Failed, err.message);
   }
 }
 
