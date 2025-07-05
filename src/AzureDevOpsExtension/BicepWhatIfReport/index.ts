3cf511ca842f235cea30db614b296628db1b98bb
Scott Anderson <scoanderson@soundphysicians.com>
2025-07-05 12:36:12 -0700

    Working on directory recognition

--- src/AzureDevOpsExtension/BicepWhatIfReport/index.ts
+++ src/AzureDevOpsExtension/BicepWhatIfReport/index.ts
@@ -1,11 +1,12 @@
 
-//import * as fs from 'fs';
-//import * as path from 'path';
+import * as fs from 'fs';
+import * as path from 'path';
 
 // Entry point for Azure DevOps Extension
 import tl = require('azure-pipelines-task-lib/task');
 import { parseWhatIfJson } from './services/parseWhatIfJson';
 import { generateReport } from './reports/generateReport';
+//import { get } from 'http';
 
 async function run() {
   try {
@@ -14,6 +15,37 @@ async function run() {
     //const inputString = fs.readFileSync(inputFilePath, 'utf8');
     let parsed: object;
     let report: Promise<string>;
+    let baseDirectory: string;
+
+    // Get the base directory from the task input
+    const markdownPath: string | undefined = tl.getInput('markdownPath', true);
+    if (!markdownPath) {
+      tl.setResult(tl.TaskResult.Failed, 'Markdown path is required.');
+      return;
+    } else {
+      //baseDirectory = markdownPath;
+      tl.debug(`Markdown path input: ${markdownPath}`);
+    }
+
+    // Check if the provided path is absolute
+    if (!path.isAbsolute(markdownPath)) {
+      // Convert relative path to absolute path
+      baseDirectory = path.join(tl.getVariable('System.DefaultWorkingDirectory') || '', markdownPath);
+      tl.debug(`Converted relative path to absolute path: ${baseDirectory}`);
+    } else {
+      // Use the absolute path as is
+      baseDirectory = markdownPath;
+      tl.debug(`Base directory is already absolute: ${baseDirectory}`);
+    }
+    // Check if the base directory exists
+    if (!fs.statSync(baseDirectory).isDirectory()) {
+      tl.setResult(tl.TaskResult.Failed, `The provided path is not a directory: ${baseDirectory}`);
+      return;
+    } else {
+      tl.debug(`Base directory exists: ${baseDirectory}`);
+    }
+
+    
 
     const inputString: string | undefined = tl.getInput('bicepWhatIfJSON', true);
     if (inputString === undefined) {
@@ -50,3 +82,13 @@ run()
   //    tl.setResult(tl.TaskResult.Failed, `err: ${err}`);
   //  }
   //});
+
+  function getPredefinedVariable(name: string) {
+    name = replaceAll(name, ".", "_");
+    name = replaceAll(name, " ", "_");
+    return process.env[name];
+}
+
+function replaceAll(text: string, pattern: string, replacement: string) {
+    return text.split(pattern).join(replacement);
+}
