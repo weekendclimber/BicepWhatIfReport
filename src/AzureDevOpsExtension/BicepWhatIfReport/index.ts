8d0530cf81a035665a050813a16acebed7ce2c4b
Scott Anderson <scoanderson@soundphysicians.com>
2025-07-05 14:36:23 -0700

    Refactor Bicep What-If report generation and parsing logic
    - Updated the run function to handle multiple JSON files and generate corresponding Markdown reports.
    - Improved error handling for file existence and directory checks.
    - Enhanced the parseWhatIfJson function to read from a file and parse its content.
    - Adjusted the generateReport function to await the parsed data before generating the report.
    - Completes Generate Human-Readable Report #2
    - Starts Integrate Report Generation in Azure DevOps Extension #5

--- src/AzureDevOpsExtension/BicepWhatIfReport/index.ts
+++ src/AzureDevOpsExtension/BicepWhatIfReport/index.ts
@@ -8,58 +8,91 @@ import { parseWhatIfJson } from './services/parseWhatIfJson';
 import { generateReport } from './reports/generateReport';
 //import { get } from 'http';
 
+const ATTACHMENT_TYPE: string = 'bicepwhatifreport';
+
 async function run() {
   try {
     //const inputFilePath = path.resolve(__dirname, '../../../tests/AzureDevOpsExtension/report.json');
     //const outputFilePath = path.resolve(__dirname, 'test.md');
     //const inputString = fs.readFileSync(inputFilePath, 'utf8');
-    let parsed: object;
-    let report: Promise<string>;
-    let baseDirectory: string;
+    let reports: string[] = [];
+    let baseDirectory: string = '';
 
     // Get the base directory from the task input
-    const markdownPath: string | undefined = tl.getInput('markdownPath', true);
-    if (!markdownPath) {
+    const whatIfJSONPath: string | undefined = tl.getInput('bicepWhatIfJSONPath', true);
+    if (!whatIfJSONPath) {
+      tl.debug('Markdown path input is not provided.');
       tl.setResult(tl.TaskResult.Failed, 'Markdown path is required.');
       return;
     } else {
-      //baseDirectory = markdownPath;
-      tl.debug(`Markdown path input: ${markdownPath}`);
+      tl.debug(`Markdown path input: ${whatIfJSONPath}`);
     }
-
     // Check if the provided path is absolute
-    if (!path.isAbsolute(markdownPath)) {
+    if (!path.isAbsolute(whatIfJSONPath)) {
       // Convert relative path to absolute path
-      baseDirectory = path.join(tl.getVariable('System.DefaultWorkingDirectory') || '', markdownPath);
+      baseDirectory = path.join(tl.getVariable('System.DefaultWorkingDirectory') || '', whatIfJSONPath);
       tl.debug(`Converted relative path to absolute path: ${baseDirectory}`);
     } else {
       // Use the absolute path as is
-      baseDirectory = markdownPath;
+      baseDirectory = whatIfJSONPath;
       tl.debug(`Base directory is already absolute: ${baseDirectory}`);
     }
     // Check if the base directory exists
     if (!fs.statSync(baseDirectory).isDirectory()) {
+      tl.debug(`The provided path is not a directory: ${baseDirectory}`);
       tl.setResult(tl.TaskResult.Failed, `The provided path is not a directory: ${baseDirectory}`);
       return;
     } else {
       tl.debug(`Base directory exists: ${baseDirectory}`);
     }
 
-    
-
-    const inputString: string | undefined = tl.getInput('bicepWhatIfJSON', true);
-    if (inputString === undefined) {
-      const errorMessage = 'No JSON input was given!';
-      //tl.setResult(tl.TaskResult.Failed, errorMessage);
-      throw new Error(`Error: ${errorMessage}`);
+    // Get all JSON files in the base directory, assuming that they are the bicep what-if reports
+    const jsonFiles: string[] = await getFiles(baseDirectory);
+    if (!jsonFiles || jsonFiles.length === 0) {
+      tl.debug(`No JSON files found in the directory: ${baseDirectory}`);
+      tl.setResult(tl.TaskResult.Succeeded, `No JSON files found in the directory: ${baseDirectory}`);
+      return;
+    } else {
+      tl.debug(`Found JSON '${jsonFiles.length}' files:\n\t${jsonFiles.join(`\n\t`)}`);
     }
 
-    // Parse the what-if JSON
-    parsed = parseWhatIfJson(inputString as string);
-    // Generate a human-readable report
-    report = generateReport(parsed);
-    //fs.writeFileSync(outputFilePath, await report, 'utf-8');
-    return tl.setResult(tl.TaskResult.Succeeded, await report); 
+    await Promise.all(jsonFiles.map(async file => {
+      const filePath: string = path.join(baseDirectory, file);
+      const outputFilePath: string = path.join(baseDirectory, `${file.replace('.json', '.md')}`);
+      let report: string;
+
+      // Check if the file exists
+      if (!fs.existsSync(filePath)) {
+        tl.setResult(tl.TaskResult.Failed, `The file '${file}' does not exist at path: ${filePath}`);
+        return;
+      } else {
+        tl.debug(`Parsing file '${file}' at path: ${filePath}`);
+        let parsed: object = await parseWhatIfJson(filePath as string);
+        tl.debug(`Generating report for file: ${file}`);
+        report = await generateReport(parsed);
+      }
+      
+      // Write report to output file
+      if(!report) {
+        tl.setResult(tl.TaskResult.Failed, `Failed to generate report for file '${file}' in path: ${filePath}`);
+        return;
+      } else {
+        tl.debug(`Writing report to file: ${outputFilePath}`);
+        reports.push(outputFilePath);
+        fs.promises.writeFile(outputFilePath, report, 'utf-8');
+      }
+    }));
+
+    // Add attachments for all generated reports
+    if (reports.length > 0) {
+      tl.debug(`Adding attachments for '${reports.length}' generated reports:\n\t${reports.join(`\n\t`)}`);
+      addAttachments(reports, baseDirectory);
+    }
+
+    // All reports have been parsed, generate, written, and attached.
+    tl.debug(`Generated reports for '${reports.length}' files:\n\t${reports.join(`\n\t`)}`);
+    tl.setResult(tl.TaskResult.Succeeded, `Generated reports for '${reports.length}' files:\n\t${reports.join(`\n\t`)}`);
+    return;
   } catch (err: any) {
     if (err instanceof Error) {
       console.error(`Error: ${err.message}`);
@@ -83,12 +116,38 @@ run()
   //  }
   //});
 
-  function getPredefinedVariable(name: string) {
-    name = replaceAll(name, ".", "_");
-    name = replaceAll(name, " ", "_");
-    return process.env[name];
+// Function to get all JSON files in a directory
+async function getFiles(dir: string): Promise<string[]> {
+  const dirents = (await fs.promises.readdir(dir, { withFileTypes: true })).filter(file => file.name.endsWith('.json'));
+  const files: string[] = await Promise.all(dirents.map((dirent) => {
+      const res: string = path.resolve(dir, dirent.name);
+      return res;
+  }));
+  return Array.prototype.concat(...files);
+}
+
+function addAttachments(files: string[], baseDir: string) {
+  const absoluteFiles = files.map(file => path.resolve(baseDir, file));
+  const relativeFiles = files.map(file => path.relative(baseDir, file));
+  absoluteFiles.forEach((absoluteFile, index) => {
+      const relativeFile = relativeFiles[index];
+      const name = escapeFilename(relativeFile);
+      tl.addAttachment(ATTACHMENT_TYPE, name, absoluteFile);
+  });
+}
+
+function escapeFilename(filename: string): string {
+  const ESCAPED_CHARACTERS = '<>|:*?\\/ ';
+  const LOG_ESCAPE_CHARACTER = "^";
+  filename = "md/" + replaceAll(filename, "\\", "/");
+  const chars = LOG_ESCAPE_CHARACTER + ESCAPED_CHARACTERS;
+  for (let i = 0; i < chars.length; i++) {
+      const num = `${i}`.padStart(2, "0");
+      filename = replaceAll(filename, chars[i], `${LOG_ESCAPE_CHARACTER}${num}`);
+  }
+  return filename;
 }
 
 function replaceAll(text: string, pattern: string, replacement: string) {
-    return text.split(pattern).join(replacement);
+  return text.split(pattern).join(replacement);
 }
